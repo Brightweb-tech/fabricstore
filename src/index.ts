29921b6b91b517b0e144072fe8959984cf3d22f9
luisteles-bw <luis.teles@brightweb.tech>
2024-10-28 04:43:34 +0000

    removed commented line

--- src/index.ts
+++ src/index.ts
@@ -2298,7 +2298,7 @@ window.Webflow.push(() => {
       selectorValues.email = emailInput.value;
       selectorValues.contacto = contactoSwitch.checked;
       const txtBytes = await generateTxt();
-      // sendQuoteDataWhenDownload(txtBytes);
+      sendQuoteDataWhenDownload(txtBytes);
       const { blob, pdfDoc, link } = await generateAndDownloadPdfLIB();
       files.push({ blob: blob, pdf: pdfDoc, link: link });
       files[files.length - 1].link.click();
